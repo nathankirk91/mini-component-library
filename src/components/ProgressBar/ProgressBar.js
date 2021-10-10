e24f1567bfd622be99a72cdf6af7eb0eeac98f71
Nathan Kirk <nathan.kirk91@gmail.com>
2021-10-10 17:03:03 +1100

    progress bar complete

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,64 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZE = {
+  small: {
+    "--padding": 0 + "px",
+    "--height": 8 + "px",
+    "--borderRadius": 4 + "px",
+  },
+  medium: {
+    "--padding": 0 + "px",
+    "--height": 12 + "px",
+    "--borderRadius": 4 + "px",
+  },
+  large: {
+    "--padding": 4 + "px",
+    "--height": 24 + "px",
+    "--borderRadius": 8 + "px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const style = SIZE[size];
+  return (
+    <Wrapper
+      style={style}
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <VisuallyHidden>{`progress is ${value}%`}</VisuallyHidden>
+      <ProgressFillWrapper>
+        <ProgressFill value={value} />
+      </ProgressFillWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  padding: var(--padding);
+  height: var(--height);
+  border-radius: var(--borderRadius);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const ProgressFillWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const ProgressFill = styled.div`
+  background-color: ${COLORS.primary};
+  height: 100%;
+  width: ${({ value }) => value}%;
+`;
 
 export default ProgressBar;
